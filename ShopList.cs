20bfb472dd50066dd5d5bc358d86efc2d7797c54
Alina Simionescu <alinasimionescu505@yahoo.com>
2020-12-09 14:37:06 +0200

    Add project files.

---
+++ ShopList.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using System;
+
+public class Simionescu_Alina_Lab10.Models
+{
+	public class ShopList
+	{
+	[PrimaryKey, AutoIncrement]
+	public int ID { get; set; }
+	public string Description { get; set; }
+	public DateTime Date { get; set; }
+}
+}
---
+++ Simionescu_Alina_Lab10/Simionescu_Alina_Lab10/Models/ShopList.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using System;
+
+namespace Simionescu_Alina_Lab10.Models
+{
+	public class ShopList
+{
+	[PrimaryKey, AutoIncrement]
+	public int ID { get; set; }
+	public string Description { get; set; }
+	public DateTime Date { get; set; }
+}
+}
+
